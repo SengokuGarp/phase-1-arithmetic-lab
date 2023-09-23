17d30ac1a600d35a23d7fe40cf1a706522787835
Glen <garpirungu@gmail.com>
2023-09-23 12:14:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+
+
+const numbers = [5, 10, 15, 20, 2, 7];
+const max = Math.max(...numbers);
+
+
+console.log("Challenge 1 - Multiply:", multiply); 
+console.log("Challenge 2 - Random:", random); 
+console.log("Challenge 3 - Mod:", mod); 
+console.log("Challenge 4 - Max:", max); 
